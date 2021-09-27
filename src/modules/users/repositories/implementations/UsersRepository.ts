92f9f5c13a6a657fbed2fb49232a0012c1ee2be6
Vinicius de Alcantra Barros <viniciusalcantarabarros@gmail.com>
2021-09-27 10:03:31 -0300

    Repository de Users fncionando

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -6,25 +6,30 @@ import { IUsersRepository } from '../IUsersRepository';
 
 export class UsersRepository implements IUsersRepository {
   private repository: Repository<User>;
-
+  
   constructor() {
     this.repository = getRepository(User);
   }
 
-  async findUserWithGamesById({
-    user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+  async findUserWithGamesById({user_id}: IFindUserWithGamesDTO): Promise<User> {
+    const user = await this.repository.findOneOrFail({
+      relations: ['games'],
+      where: {
+        id: user_id
+      }
+    });
+    ///user = await this.repository.findOne({id:user_id})
+    return user
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users ORDER BY first_name asc"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users WHERE first_name ILIKE $1 AND last_name ILIKE $2", [first_name, last_name]); // Complete usando raw query
   }
 }
